2b0fe1e5cbffbc09b425bd001c311d2f2adf1a7e
Naveed Ullah <31434159+naveedpaen@users.noreply.github.com>
2019-04-23 22:46:35 -0700

    2

--- src/app/Products/products.module.ts
+++ src/app/Products/products.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { AddComponent } from './add/add.component';
+import { LifecycleComponent } from './lifecycle/lifecycle.component';
 import { ProductsRoutingModule } from './product-routing.module';
 import { ShowComponent } from './show/show.component';
-import { LifecycleComponent } from './lifecycle/lifecycle.component';
 
 @NgModule({
   declarations: [AddComponent, ShowComponent, LifecycleComponent],
