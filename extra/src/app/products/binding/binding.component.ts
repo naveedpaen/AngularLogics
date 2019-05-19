c7866f0fea12ad0a749f86a1b90acde445a6b36d
Naveed Ullah <31434159+naveedpaen@users.noreply.github.com>
2019-05-19 00:56:30 -0700

    k
    k

--- extra/src/app/products/binding/binding.component.ts
+++ extra/src/app/products/binding/binding.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-binding',
   templateUrl: './binding.component.html',
   styleUrls: ['./binding.component.css']
 })
+
+
 export class BindingComponent implements OnInit {
 
   disbtn = true;
@@ -22,12 +25,22 @@ export class BindingComponent implements OnInit {
   fontStyle = 'italic';
   fontSize = '20';
   stdList: any[];
+  myList: any;
 
   // Null, False , '', undefined, 0
   PageHeader = 0;
-  constructor() { }
 
-  ngOnInit() {
+  @Output myEventName 
+  constructor() { }
+  ngOnInit(): void {
+
+
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
+
+  onInit() {
 
     // this.stdList = [
     //   { id: '1', name: 'Moon', email: 'def@gmail' },
@@ -35,29 +48,19 @@ export class BindingComponent implements OnInit {
     // ];
 
 
+    this.myList = [
+      { id: '1', name: 'naveed', gender: 'male' },
+      { id: '2', name: 'maryam', gender: 'female' },
+      { id: '3', name: 'ali', gender: 'male' },
+    ]
 
 
   }
 
+}i', gender: 'male' },
+    ]
 
 
-  addMyClasses() {
-    const myClasses = {
-      italic: this.applyItalicClass,
-      bold: this.applyBoldClass
-    };
-    return myClasses;
   }
-
-  addMyStyles() {
-    const addStyles = {
-      'fontSize.px': this.fontSize,
-      'font-style': this.fontStyle,
-      'font-weight': this.fontWeight
-    };
-    return addStyles;
-  }
-
-
 
 }
