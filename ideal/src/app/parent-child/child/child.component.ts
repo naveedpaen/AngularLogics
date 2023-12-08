5cd318949e8468a2ae3ee321f4dd99359841f454
Naveed Ullah <31434159+naveedpaen@users.noreply.github.com>
2023-12-08 20:52:05 +0500

    updated

--- ideal/src/app/parent-child/child/child.component.ts
+++ ideal/src/app/parent-child/child/child.component.ts
@@ -11,6 +11,44 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 		<ng-template let-a let-id>
 			<span> Id: {{ id }}</span>
 			<span>{{ a }}</span> <br />
+		</ng-template>
+
+		<!-- Dashboard Component Template -->
+		<!-- <div *ngFor="let widget of dashboardWidgets; let index = index">
+			<ng-container *ngTemplateOutlet="widget?.template; context: { $implicit: widget.data, index: index }"></ng-container>
+		</div> -->
+
+		<!-- Dynamic Widget Templates -->
+		<ng-template #temperatureWidget let-data let-index="index">
+			<div>
+				<p>Widget {{ index + 1 }}</p>
+				<p>Temperature: {{ data.temperature }}</p>
+				<!-- Additional widget-specific content -->
+			</div>
+		</ng-template>
+
+		<ng-template #stockWidget let-data let-index="index">
+			<div>
+				<p>Widget {{ index + 1 }}</p>
+				<p>Stock Symbol: {{ data.symbol }}</p>
+				<!-- Additional widget-specific content -->
+			</div>
+		</ng-template>
+
+		<ng-template #currencyWidget let-data let-index="index">
+			<div>
+				<p>Widget {{ index + 1 }}</p>
+				<p>Currency Code: {{ data.currencyCode }}</p>
+				<!-- Additional widget-specific content -->
+			</div>
+		</ng-template>
+
+		<ng-template #newsWidget let-data let-index="index">
+			<div>
+				<p>Widget {{ index + 1 }}</p>
+				<p>News Headline: {{ data.headline }}</p>
+				<!-- Additional widget-specific content -->
+			</div>
 		</ng-template> `,
 	styleUrls: ['./child.component.css'],
 	changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,4 +56,11 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 export class ChildComponent {
 	@Input() id: number = 0;
 	@Input() name: string = '';
+
+	dashboardWidgets = [
+		{ template: 'temperatureWidget', data: { temperature: 25 } },
+		{ template: 'stockWidget', data: { symbol: 'AAPL' } },
+		{ template: 'currencyWidget', data: { currencyCode: 'USD' } },
+		{ template: 'newsWidget', data: { headline: 'Breaking News!' } }
+	];
 }
